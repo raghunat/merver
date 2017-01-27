226bb2d1510034de6f6116b642f427cf4a3f7e42
Stephen Raghunath <stephen.raghunath@acquia.com>
2017-01-27 07:55:22 -0500

    Adding delay in response for more lifelike usage

--- routes/merve.js
+++ routes/merve.js
@@ -41,69 +41,72 @@ module.exports = function(app) {
    * @param res
    */
   let merve = function(req, res) {
-    if (!app.merver.definition) {
-      return res.status(400).json({
-        code: app.constants.CODE_MISSING_ARGUMENT,
-        message: 'You have not setup merver yet. Please POST /_setup or /_setup/file'
-      });
-    }
-
-    // On request find matching route
-    let currentUrl = Object.keys(app.merver.definition.routes).find(r => r === req.baseUrl);
-    let route = currentUrl ? app.merver.definition.routes[currentUrl] : undefined;
-
-    // send back if not found
-    if (!route) {
-      return res.status(404).json({
-        code: app.constants.CODE_NOT_FOUND,
-        message: `Merver could not find ${currentUrl} in your definition`,
-        definition: app.merver.definition
-      });
-    } else {
-      let item = route[req.method];
-      // Handle multiple responses
-      if (item.response) {
-        performUpdateIfNeeded(item);
-        return res.status(item.status || 200).json(item.response);
-      } else if (item.responses) {
-        let currentResponse;
-
-        for (let i = 0; i < item.responses.length; i++) {
-          currentResponse = item.responses[i];
-
-          // Check for iterations or infinite items for response looping
-          if (!currentResponse.infinite && currentResponse.used && (!currentResponse.times || currentResponse.times === currentResponse.used)) {
-            currentResponse = undefined; // reset
-            continue;
-          }
-
-          // break if okay to use
-          currentResponse.used = currentResponse.used || 0;
-          currentResponse.used++;
-          break;
-        }
-
-        // Route response is found
-        if (currentResponse) {
-          performUpdateIfNeeded(currentResponse);
-          return res.status(currentResponse.status || 200).json(currentResponse.response);
-        } else {
-
-          // Could not find the response
-          return res.status(404).json({
-            code: app.constants.CODE_NOT_FOUND,
-            message: `Merver could not find ${req.baseUrl} in your definition with enough data`,
-            definition: app.merver.definition
-          });
-        }
-      } else {
-        // Bad definition
+    // Simulate http
+    setTimeout(() => {
+      if (!app.merver.definition) {
         return res.status(400).json({
           code: app.constants.CODE_MISSING_ARGUMENT,
-          message: `Your definition is missing the response|responses attribute on ${req.baseUrl}`
+          message: 'You have not setup merver yet. Please POST /_setup or /_setup/file'
         });
       }
-    }
+
+      // On request find matching route
+      let currentUrl = Object.keys(app.merver.definition.routes).find(r => r === req.baseUrl);
+      let route = currentUrl ? app.merver.definition.routes[currentUrl] : undefined;
+
+      // send back if not found
+      if (!route) {
+        return res.status(404).json({
+          code: app.constants.CODE_NOT_FOUND,
+          message: `Merver could not find ${currentUrl} in your definition`,
+          definition: app.merver.definition
+        });
+      } else {
+        let item = route[req.method];
+        // Handle multiple responses
+        if (item.response) {
+          performUpdateIfNeeded(item);
+          return res.status(item.status || 200).json(item.response);
+        } else if (item.responses) {
+          let currentResponse;
+
+          for (let i = 0; i < item.responses.length; i++) {
+            currentResponse = item.responses[i];
+
+            // Check for iterations or infinite items for response looping
+            if (!currentResponse.infinite && currentResponse.used && (!currentResponse.times || currentResponse.times === currentResponse.used)) {
+              currentResponse = undefined; // reset
+              continue;
+            }
+
+            // break if okay to use
+            currentResponse.used = currentResponse.used || 0;
+            currentResponse.used++;
+            break;
+          }
+
+          // Route response is found
+          if (currentResponse) {
+            performUpdateIfNeeded(currentResponse);
+            return res.status(currentResponse.status || 200).json(currentResponse.response);
+          } else {
+
+            // Could not find the response
+            return res.status(404).json({
+              code: app.constants.CODE_NOT_FOUND,
+              message: `Merver could not find ${req.baseUrl} in your definition with enough data`,
+              definition: app.merver.definition
+            });
+          }
+        } else {
+          // Bad definition
+          return res.status(400).json({
+            code: app.constants.CODE_MISSING_ARGUMENT,
+            message: `Your definition is missing the response|responses attribute on ${req.baseUrl}`
+          });
+        }
+      }
+    }, process.env.MERVER_TIMEOUT || 1000);
   };
 
   // Cycle through methods and merve
